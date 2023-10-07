1626ce2d01791c58a0e0b7cb6aa94d9e5861bf31
samandar-me <samandarcoderdev@gmail.com>
2023-10-07 15:36:45 +0500

    blah

--- lib/screen/add_update_screen.dart
+++ lib/screen/add_update_screen.dart
@@ -21,64 +21,69 @@ class _AddUpdateScreenState extends State<AddUpdateScreen> {
       appBar: AppBar(
         title: Text("Yo'l belgisini qo'shish"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  _getImageFromGallery();
-                },
-                child: Container(
-                  height: 150,
-                  width: 150,
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 2,color: Colors.grey),
-                    borderRadius: const BorderRadius.all(Radius.circular(6))
-                  ),
-                  child: Center(
-                    child: _file == null ? const Icon(Icons.image) : Image.file(_file!,fit: BoxFit.cover,),
-                  ),
-                ),
-              ),
-              const  SizedBox(height: 10),
-              Text(
-                "Yo’l belgisining rasmini qo’shish uchun rasm ustiga bosing"
-              ),
-              const SizedBox(height: 20),
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.black12,
-                    hintText: 'Nomi',
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    _getImageFromGallery();
+                  },
+                  child: Container(
+                    height: 150,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2,color: Colors.grey),
+                      borderRadius: const BorderRadius.all(Radius.circular(6))
+                    ),
+                    child: Center(
+                      child: _file == null ? const Icon(Icons.image) : Image.file(_file!,fit: BoxFit.cover,),
+                    ),
                   ),
                 ),
-              ),
-              Expanded(
-                flex: 3,
-                child: TextField(
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(vertical: 100,horizontal: 12),
-                    fillColor: Colors.black12,
+                const  SizedBox(height: 10),
+                Text(
+                  "Yo’l belgisining rasmini qo’shish uchun rasm ustiga bosing"
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  height: 60,
+                  child: TextField(
+                    decoration: InputDecoration(
                       filled: true,
-                      hintText: "Ta'rifi"
-                  ),
-                ),
-              ),
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
                       fillColor: Colors.black12,
-                      filled: true,
-                      hintText: "Qaysi turga kirishi"
+                      hintText: 'Nomi',
+                    ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 300,
+                  child: TextField(
+                    textAlign: TextAlign.start,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(vertical: 100,horizontal: 12),
+                      fillColor: Colors.black12,
+                        filled: true,
+                        hintText: "Ta'rifi"
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 60,
+                  child: TextField(
+                    decoration: InputDecoration(
+                        fillColor: Colors.black12,
+                        filled: true,
+                        hintText: "Qaysi turga kirishi"
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
